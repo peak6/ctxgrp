cfc4e48c0e0a2b3364711eb60fd4a858848d4e6e
David Budworth <dbudworth@peak6.com>
2016-03-14 13:18:32 -0700

    fixed bug in copy

--- middleware.go
+++ middleware.go
@@ -5,16 +5,12 @@ type MiddlewareFunc func(hf Handler) Handler
 
 func copyof(src Middleware) Middleware {
 	dest := make(Middleware, len(src))
-	copy(dest, dest)
+	copy(dest, src)
 	return dest
 }
 
 func (m Middleware) Use(fns ...MiddlewareFunc) Middleware {
-	ret := copyof(m)
-	for _, fn := range fns {
-		ret = append(ret, fn)
-	}
-	return ret
+	return append(copyof(m), fns...)
 }
 
 // Then creates a handler with all the middleware applied
